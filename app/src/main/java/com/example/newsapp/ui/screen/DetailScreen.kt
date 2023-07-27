786a50c88e3135735443c18eee5ac5a1713983ac
tortamque <90132962+tortamque@users.noreply.github.com>
2023-07-27 23:45:31 +0300

    Create UI for DetailScreen

--- app/src/main/java/com/example/newsapp/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/screen/DetailScreen.kt
@@ -1,17 +1,31 @@
 package com.example.newsapp.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.newsapp.R
@@ -23,14 +37,70 @@ fun DetailScreen(
     newsData: NewsData,
     scrollState: ScrollState
 ){
-    Column(modifier = Modifier.fillMaxWidth().verticalScroll(scrollState), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Detail Screen", fontWeight = FontWeight.SemiBold)
-        Button(onClick = {
-            navController.popBackStack()
-        }) {
-            Text("Go to Top News Screen")
+    val defaultPadding = 20.dp
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+            .padding(horizontal = 20.dp),
+    ) {
+        Text(
+            text = newsData.title,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier
+                .align(CenterHorizontally)
+                .padding(vertical = defaultPadding)
+        )
+
+        Row(
+            Modifier.padding(bottom = defaultPadding/2)
+        ) {
+            Text(
+                newsData.author,
+                color = Color.Black,
+                modifier = Modifier.weight(1.0f),
+                fontSize = 16.sp
+            )
+            Text(
+                newsData.publishedAt,
+                color = Color.Gray,
+                modifier = Modifier
+                    .weight(1.0f)
+                    .align(Alignment.Bottom),
+                textAlign = TextAlign.End,
+                fontSize = 14.sp
+            )
         }
-        Text("News title: ${newsData.title}")
+
+        Image(
+            painter = painterResource(id = newsData.imageId),
+            contentDescription = newsData.title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(bottom = defaultPadding)
+                .clip(RoundedCornerShape(10.dp))
+        )
+
+        Text(
+            newsData.description,
+            Modifier
+                .padding(bottom = defaultPadding)
+        )
+
+        Button(
+            onClick = {
+                navController.popBackStack()
+            },
+            modifier = Modifier
+                .padding(bottom = defaultPadding)
+                .align(CenterHorizontally)
+        ) {
+            Text("Go Back")
+        }
     }
 }
 
