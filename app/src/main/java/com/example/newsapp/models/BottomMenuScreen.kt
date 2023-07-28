f2bfb7d8ebf1ebfe8b2013776ae3378dba30e5af
tortamque <90132962+tortamque@users.noreply.github.com>
2023-07-28 17:13:39 +0300

    Change Sources icon

--- app/src/main/java/com/example/newsapp/models/BottomMenuScreen.kt
+++ app/src/main/java/com/example/newsapp/models/BottomMenuScreen.kt
@@ -3,10 +3,11 @@ package com.example.newsapp.models
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Category
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.List
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class BottomMenuScreen(val route: String, val icon: ImageVector, val title: String){
     object TopNews: BottomMenuScreen("TopNews", Icons.Outlined.Home, "Top News")
     object Categories: BottomMenuScreen("Categories", Icons.Outlined.Category, "Categories")
-    object Sources: BottomMenuScreen("Sources", Icons.Outlined.Home, "Sources")
+    object Sources: BottomMenuScreen("Sources", Icons.Outlined.List, "Sources")
 }
