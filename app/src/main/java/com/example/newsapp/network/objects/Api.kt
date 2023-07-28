d2e028b538ef5517c7f4c915b07f1370a367dfa8
tortamque <90132962+tortamque@users.noreply.github.com>
2023-07-28 22:42:27 +0300

    Add Moshi support

--- app/src/main/java/com/example/newsapp/network/objects/Api.kt
+++ app/src/main/java/com/example/newsapp/network/objects/Api.kt
@@ -1,12 +1,20 @@
 package com.example.newsapp.network.objects
 
 import com.example.newsapp.network.services.NewsService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 object Api {
     private const val BASE_URL = "https://newsapi.org/v2/"
 
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
     private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .baseUrl(BASE_URL)
         .build()
 
