0d2c49d56e17c2da3e3671f483b9fa9ac198b929
tortamque <90132962+tortamque@users.noreply.github.com>
2023-07-28 19:49:03 +0300

    Create NewsService interface

---
+++ app/src/main/java/com/example/newsapp/models/services/NewsService.kt
@@ -0,0 +1,11 @@
+package com.example.newsapp.models.services
+
+import com.example.newsapp.models.repository.TopNewsResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsService {
+    @GET("top-headlines")
+    fun getTopArticles(@Query("country") country: String, @Query("apiKey") apiKey: String): Call<TopNewsResponse>
+}
