c63ddaf8d67687722885e5fce68bb17a6d0d8c6d
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-02 19:56:29 +0300

    Implement query and source search

--- app/src/main/java/com/example/newsapp/ui/MainViewModel.kt
+++ app/src/main/java/com/example/newsapp/ui/MainViewModel.kt
@@ -7,6 +7,7 @@ import com.example.newsapp.MainApp
 import com.example.newsapp.data.models.ArticleCategory
 import com.example.newsapp.data.models.getCategory
 import com.example.newsapp.data.models.repository.TopNewsResponse
+import com.example.newsapp.network.models.SourcesEnum
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,6 +16,7 @@ import kotlinx.coroutines.launch
 class MainViewModel(application: Application): AndroidViewModel(application) {
     private val repository = getApplication<MainApp>().repository
 
+    // Category
     private val _selectedCategory = MutableStateFlow(ArticleCategory.ALL_NEWS)
     val selectedCategory: StateFlow<ArticleCategory>
         get() = _selectedCategory
@@ -28,6 +30,7 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
 
     fun getArticlesByCategory(category: String){
         _isLoading.value = true
+
         viewModelScope.launch(Dispatchers.IO){
             _categoryNewsResponse.value = repository.getArticlesByCategory(category)
             _isLoading.value = false
@@ -37,5 +40,36 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
     fun onSelectedCategoryChanged(category: ArticleCategory){
         val newCategory = getCategory(category.categoryName)
         _selectedCategory.value = newCategory
+    }
+
+    // Source
+    val sourceName = MutableStateFlow(SourcesEnum.NBCNews)
+    private val _sourceNewsResponse = MutableStateFlow(TopNewsResponse())
+    val sourceNewsResponse: StateFlow<TopNewsResponse>
+        get() = _sourceNewsResponse
+
+    fun getArticlesBySource(){
+        _isLoading.value = true
+
+        viewModelScope.launch(Dispatchers.IO){
+            _sourceNewsResponse.value = repository.getArticlesBySource(sourceName.value.sourceKey)
+            _isLoading.value = false
+        }
+    }
+
+
+    // Query
+    val query = MutableStateFlow("")
+    private val _queryNewsResponse = MutableStateFlow(TopNewsResponse())
+    val queryNewsResponse: StateFlow<TopNewsResponse>
+        get() = _queryNewsResponse
+
+    fun getArticlesByQuery(query: String){
+        _isLoading.value = true
+
+        viewModelScope.launch(Dispatchers.IO){
+            _queryNewsResponse.value = repository.getArticlesByQuery(query)
+            _isLoading.value = false
+        }
     }
 }
