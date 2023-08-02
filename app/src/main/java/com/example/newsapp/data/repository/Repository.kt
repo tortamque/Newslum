678285566735cf0d2130d84ee12e923bc44f239c
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-02 19:55:14 +0300

    Add MVVM support for Repository

--- app/src/main/java/com/example/newsapp/data/repository/Repository.kt
+++ app/src/main/java/com/example/newsapp/data/repository/Repository.kt
@@ -4,4 +4,6 @@ import com.example.newsapp.network.models.NewsManager
 
 class Repository(val manager: NewsManager){
     suspend fun getArticlesByCategory(category: String) = manager.getArticlesByCategory(category)
+    suspend fun getArticlesBySource(source: String) = manager.getArticlesBySource(source)
+    suspend fun getArticlesByQuery(query: String) = manager.getArticlesByQuery(query)
 }
