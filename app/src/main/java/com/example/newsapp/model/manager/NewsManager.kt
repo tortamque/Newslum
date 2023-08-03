0f5da60413e75246bd471744c04c269134bd5670
tortamque <90132962+tortamque@users.noreply.github.com>
2023-08-03 12:54:31 +0300

    Delete unnecessary fields

--- app/src/main/java/com/example/newsapp/model/manager/NewsManager.kt
+++ app/src/main/java/com/example/newsapp/model/manager/NewsManager.kt
@@ -1,9 +1,6 @@
 package com.example.newsapp.model.manager
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import com.example.newsapp.model.models.network.TopNewsResponse
-import com.example.newsapp.model.enums.network.SourcesEnum
 import com.example.newsapp.model.service.NewsService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -11,14 +8,6 @@ import kotlinx.coroutines.withContext
 class NewsManager(
     private val service: NewsService
 ){
-    val sourceName = mutableStateOf(SourcesEnum.NBCNews)
-    private val _getArticleBySource = mutableStateOf(TopNewsResponse())
-    val getArticlesBySource: MutableState<TopNewsResponse> = _getArticleBySource
-
-    val query = mutableStateOf("")
-    private val _searchNewsResponse = mutableStateOf(TopNewsResponse())
-    val searchNewsResponse: MutableState<TopNewsResponse> = _searchNewsResponse
-
     suspend fun getArticlesByCategory(category: String): TopNewsResponse = withContext(Dispatchers.IO){
         service.getArticlesByCategory(category = category, country = "us")
     }
